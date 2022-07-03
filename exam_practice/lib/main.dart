3cc087a420948dbb5c622bbae2cd2c2e29c2427f
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-07-03 15:22:09 +0500

    row-column-structure

--- exam_practice/lib/main.dart
+++ exam_practice/lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -16,14 +15,34 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepOrange,
           title:const Text("Exam Practice")
         ),
-        body: Container(
-          child: const Text("Some Text here.."),
-          margin: const EdgeInsets.all(100),
-          padding: const EdgeInsets.all(20),
-          color: Colors.red,
-          height: 100,
-          width: 100,
-        )
+        body: 
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text("Icons in Column", style: TextStyle(fontSize: 20),),
+            Icon(Icons.abc),
+            Icon(Icons.ac_unit),
+            Icon(Icons.alarm),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+              Text("Icons in Row", style: TextStyle(fontSize: 20)),
+              Icon(Icons.mail),
+              Icon(Icons.add),
+              Icon(Icons.message)
+            ],
+            ),
+          ],
+        ),
+        // Container(
+        //   child: const Text("Some Text here.."),
+        //   margin: const EdgeInsets.all(100),
+        //   padding: const EdgeInsets.all(20),
+        //   color: Colors.red,
+        //   height: 100,
+        //   width: 100,
+        // )
       ),
     );
   }

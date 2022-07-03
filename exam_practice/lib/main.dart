f117ee326579bbd140f98ba945e120a046d089c0
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-07-03 22:01:33 +0500

    navigation

--- exam_practice/lib/main.dart
+++ exam_practice/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
+//import 'dart:math' as math;
 
 void main() {
-  runApp(const MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,12 +10,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+        return MaterialApp(
+      title: 'Exams Practice',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.indigo,
           title:const Text("Exam Practice")
         ),
+
         // floatingActionButton: FloatingActionButton(onPressed:(){}, child: const Icon(Icons.add),),
         // bottomNavigationBar: BottomNavigationBar(items: const [
         //   BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
@@ -27,16 +41,24 @@ class MyApp extends StatelessWidget {
         //   child: Center(child: Text("Hello from Drawer",style: TextStyle(fontSize: 20),),),
         // ),
 
-        body: 
-          ListView.builder(
-            itemBuilder: (_,index){
-              return Container(
-                color: randomColor(),
-                width: 100,
-                height: 100,
-              );
-          },
+        body:  
+          ElevatedButton(
+            child: const Text("Navigate"),
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> const ScreenTwo()));
+            },
           )
+
+        //====================================
+          // ListView.builder(
+          //   itemBuilder: (_,index){
+          //     return Container(
+          //       color: randomColor(),
+          //       width: 100,
+          //       height: 100,
+          //     );
+          // },
+          // )
 
         //======================================
         // ListView(
@@ -144,11 +166,28 @@ class MyApp extends StatelessWidget {
         //   width: 100,
         // )
 
-      ),
     );
   }
   
-  randomColor() {
-    return Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  // randomColor() {
+  //   return Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  // }
+}
+
+class ScreenTwo extends StatelessWidget {
+  const ScreenTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Screen-Two"),
+        backgroundColor: Colors.green,
+      ),
+      body: 
+        const Center(
+          child: Text("Hello form Screen-Two", style: TextStyle(fontSize: 20),),
+        ),
+    );
   }
 }

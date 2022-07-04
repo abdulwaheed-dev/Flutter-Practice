ba3d8c881d4408ef223d57ba47495d587e874ae6
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-07-04 23:39:09 +0500

    lab-exam

---
+++ final_lab_exam/android/app/src/main/kotlin/com/example/abdulwaheed_final_lab_exam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.abdulwaheed_final_lab_exam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ final_lab_exam/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Final Exam"),
+      ),
+      body: Center(child: const Text("Hello World")),
+    );
+  }
+}
